358c8817deebddca0862d8c778feef391fb47a22
Don Neeleman <donneeleman@gmail.com>
2016-04-05 12:29:06 +0200

    A little bit of documentation to check.

---
+++ src/ParkingPass.java
@@ -0,0 +1,6 @@
+/*
+* In this class the program will check if the car owner is a parking pass holder
+* If the owner is a parking pass holder, he can drive through without to pay.
+* But the owner will be accounted for on the end of his parking and will be charged on
+* his account.
+*/
