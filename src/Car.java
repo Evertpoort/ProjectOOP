f5ffd95da86f8c9f6591adfbdb8ca240535b99b2
Evert <Evert@145.37.152.76>
2016-04-11 12:48:49 +0200

    Hoi

---
+++ src/Car.java
@@ -0,0 +1,52 @@
+
+
+public abstract class Car {
+
+    private Location location;
+    private int minutesLeft;
+    private boolean isPaying;
+    
+    
+    
+
+    /**
+     * Constructor for objects of class Car /
+     */
+    public Car() {
+
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public int getMinutesLeft() {
+        return minutesLeft;
+    }
+
+    public void setMinutesLeft(int minutesLeft) {
+        this.minutesLeft = minutesLeft;
+    }
+    
+    public int getMinutesStayed(int minutesLeft)
+    {
+    	return minutesLeft;
+    }
+        
+    public boolean getIsPaying() {
+        return isPaying;
+    }
+
+    public void setIsPaying(boolean isPaying) {
+        this.isPaying = isPaying;
+    }
+
+    public void tick() {
+        minutesLeft--;
+    }
+
+}
